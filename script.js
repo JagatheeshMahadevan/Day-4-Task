8fcd7e64e54c9b4799b352eb7db1bb25bd3c549f
JagatheeshMahadevan <118879080+JagatheeshMahadevan@users.noreply.github.com>
2023-01-31 09:11:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// 1st Question:
+// To compare two JSON have the same properties without order
+
+var obj1 = {name: "person 1", age:5}
+var obj2 = {age :5, name: "person 2"};
+console.log(JSON.stringify(obj1,obj2));
+
+
+//---------------------------------------------------------
+
+//2nd Question:
+// Q2 use the rest countries' API URL and display all the country flags  in the console.
+
+var countries = new XMLHttpRequest();
+
+countries.open("GET", "https://restcountries.com/v3.1/all");
+
+countries.send();
+
+countries.onload = function () {
+  var data = countries.response;
+
+
+var result = JSON.parse(data);
+for(i=0; i<result.length; i++) {
+    console.log(result[i].flag);
+}
+}
+
+//------------------------------------------------------------
+
+//3rd Question :
+//Use the same rest countries and print all countries names, region , sub-region and populations
+
+var countries = new XMLHttpRequest();
+countries.open("GET", "https://restcountries.com/v3.1/all");
+countries.send();
+countries.onload = function () {
+  var data = countries.response;
+var result = JSON.parse(data);
+for(i=0; i<result.length; i++) {
+    console.log(result[i].name.common + " "+ result[i].region+" "+result[i].subregion+" "+result[i].population);
+}
+ }
+
+ //-------------------------------------------------------------
